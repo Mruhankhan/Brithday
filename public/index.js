98600ccf6c11a33e106cfd84d1f6b100c27adf9f
Mruhankhan <s1942405@online.houstonisd.org>
2021-11-24 15:54:12 -0600

    Work

---
+++ public/index.js
@@ -0,0 +1,9 @@
+let div = document.getElementById("div")
+div.addEventListener("click", () => {
+  let h1 = document.getElementById("h1")
+  h1.innerHTML = "Have an wonderful birthday hope you have a lots of presents "
+  let h2 = document.getElementById("h2")
+  h2.remove()
+  let h3 = document.getElementById("h3")
+  h3.remove()
+})
