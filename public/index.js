206e40cfc89125cff21f723c3b465ead6117f6d0
Mruhankhan <s1942405@online.houstonisd.org>
2021-11-25 16:58:02 -0600

    Work

--- public/index.js
+++ public/index.js
@@ -1,11 +1,7 @@
 let div = document.getElementById("div")
 div.addEventListener("click", () => {
   document.body.classList.add("add")
-  div.classList.add("")
-  let h1 = document.getElementById("h1")
-  h1.innerHTML = "Have an wonderful birthday hope you have a lots of presents "
-  let h2 = document.getElementById("h2")
-  h2.remove()
-  let h3 = document.getElementById("h3")
-  h3.remove()
+  let divNew = document.getElementById("new")
+  divNew.classList.add("new")
 })
+//
