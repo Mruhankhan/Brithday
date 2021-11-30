b1249f435d67a9317d9960843ac07e2f07d02660
Mruhankhan <s1942405@online.houstonisd.org>
2021-11-30 17:41:36 -0600

    Work

--- public/index.js
+++ public/index.js
@@ -3,5 +3,6 @@ div.addEventListener("click", () => {
   document.body.classList.add("add")
   let divNew = document.getElementById("new")
   divNew.classList.add("new")
+  divNew.innerHTML = "Have a good birthday"
 })
 //
